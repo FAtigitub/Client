4c3c288012434667528b5999039fbd0ddf95c927
Fatima <fatima.ouali2002@gmail.com>
2024-10-31 23:05:22 +0100

    commit 1

---
+++ src/main/java/ma/projet/client/entities/Client.java
@@ -0,0 +1,22 @@
+package ma.projet.client.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private Float age;
+
+
+
+}
